9e53f4710155e8dbd6218d12406a6189272db0f7
Ravi Joshi <ravi_joshi@epam.com>
2026-08-05 15:27:35 +0530

    remove spave

--- ai-translation/app.js
+++ ai-translation/app.js
@@ -2,11 +2,8 @@ import express from "express";
 import translatorRoute from "./routes/translatorRoute.js"
 
 const app = express();
-
 app.use(express.json());
-
 app.use(translatorRoute);
-
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 });
